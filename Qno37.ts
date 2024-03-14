82348aa64d806cc4d63e9a4b7305cb0559ad727d
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 05:58:28 +0500

    Add files via upload

---
+++ Qno37.ts
@@ -0,0 +1,9 @@
+// 37. Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+
+function make_shirt(size:string="",massage:string=""):void{
+    console.log(`i am making ka T-Shrit ${size} print a massage ${massage}`);
+}
+make_shirt("Large","I love typescript")
+make_shirt("medium","I love javascript")
+make_shirt("small","I love coding")
+
